26be43f4a4994a7bfb16869663932e9d13151222
rjcastaneda <roswelljcastaneda@gmail.com>
2020-02-28 23:50:27 -0800

    Fix
    Undone Changes

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Roswell James Castaneda
+ * RED ID: 820249749
+ * CS 583 Project 1
+ * FileName: MainMenuUI.cs
+ * 
+ * Script that handles all functions
+ * within the MainMenu.
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class MainMenuUI : MonoBehaviour
+{
+    private Player player;
+    private Button startButton;
+    private void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        startButton = GameObject.Find("Start_Button").GetComponent<Button>();
+        //If the Player has created a character, unlock start button.
+        if (player.startUnlocked == true)
+        {
+            startButton.interactable = true;
+        }
+    }
+    public void rollCharacter()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void startGame()
+    {
+
+        SceneManager.LoadScene(2);
+    }
+
+    public void exitGame()
+    {
+        Debug.Log("Game has Quit!");
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+}
---
+++ Assets/Scripts/UI/ReturnToMenuUI.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Roswell James Castaneda
+ * RED ID: 820249749
+ * CS 583 Project 1
+ * FileName: ReturnToMenuUI.cs
+ * 
+ * Script for return to menu button in
+ * Character creation menu.
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMenuUI : MonoBehaviour
+{
+    public void RTMButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
